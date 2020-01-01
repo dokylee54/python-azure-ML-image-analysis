4f3a227e01aefa2086927c1667c335382488b2af
Dokyung Lee <dokylee54@gmail.com>
2020-01-01 16:05:59 +0900

    file utils 200101

--- file_util.py
+++ file_util.py
@@ -10,7 +10,7 @@ def write_list_to_csv(colname_names, row_list, filename, append_check):
     else: append_check = 'w'
     
     ## open file
-    with open(filename, append_check, newline='') as file: 
+    with open(filename, append_check, newline='', encoding='utf-8') as file: 
         
         writer = csv.writer(file) 
 
@@ -18,8 +18,13 @@ def write_list_to_csv(colname_names, row_list, filename, append_check):
         if row_list != '': 
             writer.writerow(colname_names)
 
-        ## rows
-        writer.writerows(row_list)
+        ## row_list is not a list -> just one row
+        if(len(row_list)==1):
+            writer.writerow(row_list[0])
+
+        ## row_list is a list
+        else:
+            writer.writerows(row_list)
 
 
 '''
@@ -31,6 +36,21 @@ def move_file(filename, src_dir, des_dir):
 
     shutil.move(src_dir + filename, des_dir + filename)
 
+
+'''
+    parse a filename made by 'instaloader' library
+'''
+def parse_filename(f):
+
+    f_filename = f.split('/')[-1]
+    
+    temp = f_filename.split('!')[1]
+    
+    f_extension = temp[-4:]
+    f_shortcode = temp[:11]
+    f_category = temp[11:-4]
+
+    return f_filename, f_shortcode, f_category, f_extension
 
 
 if __name__ == "__main__":
